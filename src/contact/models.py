1c4be286364c1dabb18d0889692766577d9f1304
Tsung Hung <hungmasterj@gmail.com>
2014-06-07 17:18:43 -0700

    first commit

---
+++ src/contact/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+	full_name = models.CharField(max_length = 300, null=True, blank=True)
+	email = models.EmailField()
+	message = models.CharField(max_length=500)
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+	def __unicode__(self):
+		return "Message from: " + str(self.email)
